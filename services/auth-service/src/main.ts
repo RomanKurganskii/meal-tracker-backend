b2bb93a68a2e5a590f6a81d43762971f403597c3
roman kurganskii <romankurganskii@yandex.ru>
2026-01-30 13:11:14 +0300

    env.example

--- services/auth-service/src/main.ts
+++ services/auth-service/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000, () => {
-    console.log(`Auth service started on ${process.env.PORT ?? 3000} - env.${process.env.ENV ?? 'dev'}`)
+    console.log(
+      `Auth service started on ${process.env.PORT ?? 3000} - env.${process.env.NODE_ENV ?? 'dev'}`,
+    );
   });
 }
 bootstrap();
