a3a5bf7f500d999ff6f12e7bb6aed8657400db7c
roman kurganskii <romankurganskii@yandex.ru>
2026-01-30 13:19:26 +0300

    configs

---
+++ services/auth-service/src/configs/jwt-core-module.ts
@@ -0,0 +1,21 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Global()
+@Module({
+	imports: [
+		JwtModule.registerAsync({
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				global: true,
+				secret: configService.get('JWT_SECRET'),
+				signOptions: {
+					expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+				},
+			}),
+		}),
+	],
+	exports: [JwtModule],
+})
+export class JwtCoreModule {}
--- services/auth-service/src/main.ts
+++ services/auth-service/src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, () => {
+  app.useGlobalPipes(new ValidationPipe());
+  const configService: ConfigService = app.get(ConfigService);
+
+  // if (configService.get(ENV.NODE_ENV) !== 'prod') {
+  //   setupSwagger(app);
+  // }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({ origin: `*`, credentials: true });
+  app.listen(configService.get(`PORT`) ?? 3000, () =>
     console.log(
-      `Auth service started on ${process.env.PORT ?? 3000} - env.${process.env.NODE_ENV ?? 'dev'}`,
-    );
-  });
+      `Auth service started on ${configService.get(`PORT`)}`,
+    ),
+  );
 }
 bootstrap();
